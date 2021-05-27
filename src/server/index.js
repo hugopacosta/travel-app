589debcdc9b4d33564b7a1a078cf1c39c1cb4d99
Hugo P. Costa <hugopacosta@yahoo.com.br>
2021-05-27 00:02:49 -0300

    fix: asset link

--- src/server/index.js
+++ src/server/index.js
@@ -9,11 +9,11 @@ const app = express()
 dotenv.config();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname + 'public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
     // res.sendFile('dist/index.html')
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 })
 
 // designates what port the app will listen to for incoming requests
